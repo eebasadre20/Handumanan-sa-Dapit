351b14d9fb21f24e1112c28100854b1d3333f226
eebasadre <edsil@piktochart.com>
2016-10-21 23:32:12 +0800

    Working handumanan-sa-dapit app ( need to be refactor )

--- handumanan-sa-dapit/ViewController.swift
+++ handumanan-sa-dapit/ViewController.swift
@@ -7,12 +7,120 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate {
 
+    // CHECK: CLLocation documentation
+    var manager: CLLocationManager!
+    
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+
+        
+        if activePlace == -1 {
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+        } else {
+            let latitude = NSString(string: places[activePlace]["lat"]!).doubleValue
+            let longitude = NSString(string: places[activePlace]["long"]!).doubleValue
+            var coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+            
+            let latDelta:CLLocationDegrees = 0.01
+            let longDelta:CLLocationDegrees = 0.01
+            
+            var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+            
+            var region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+            
+            self.map.setRegion(region, animated: true)
+            
+            let annotation = MKPointAnnotation()
+            
+            annotation.coordinate = coordinate
+            annotation.title = places[activePlace]["name"]
+            self.map.addAnnotation(annotation)
+        }
+        
+        
+        // CHECK: UILongPressGestureRecognizer documentation
+        let uiLongPressGesture = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        uiLongPressGesture.minimumPressDuration = 2.0
+        map.addGestureRecognizer(uiLongPressGesture)
+        
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            print("I'm touching now!")
+            var touchPoint = gestureRecognizer.locationInView(self.map)
+            
+            
+            // CHECK: Convert touch point documentation
+            var newCoordinate = self.map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+            var location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            // CHECK: CLGeocoder documentation
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+                
+                var title = ""
+                
+                if( error == nil ) {
+                    if let p = placemarks?[0] {
+                        var subThoroughfare:String = ""
+                        var thoroughfare:String = ""
+                        
+                        if p.subThoroughfare != nil {
+                            subThoroughfare = p.subThoroughfare!
+                        }
+                        
+                        if p.thoroughfare != nil {
+                            thoroughfare = p.thoroughfare!
+                        }
+                        
+                        title = "\(subThoroughfare) \(thoroughfare)"
+                    }
+                }
+                
+                if title == "" {
+                    title = "Added \(NSDate())"
+                }
+                
+                places.append(["name":title, "lat":"\(newCoordinate.latitude)","long":"\(newCoordinate.longitude)"])
+                
+                // CHECK: MKPointAnnotation documentation
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = newCoordinate
+                annotation.title = title
+                self.map.addAnnotation(annotation)
+
+            })
+            
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var userLocation: CLLocation = locations[0]
+        var latitude = userLocation.coordinate.latitude
+        var longitude = userLocation.coordinate.longitude
+        
+        var coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let latDelta:CLLocationDegrees = 0.01
+        let longDelta:CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        
+        self.map.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
